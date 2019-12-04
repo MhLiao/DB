df1e029943a11ae8dd11059ac30732a7e9e76c71
MhLiao <mhliao@hust.edu.cn>
2019-12-04 19:12:10 +0800

    add zero check for L1loss

--- decoders/l1_loss.py
+++ decoders/l1_loss.py
@@ -7,8 +7,12 @@ class MaskL1Loss(nn.Module):
         super(MaskL1Loss, self).__init__()
 
     def forward(self, pred: torch.Tensor, gt, mask):
-        loss = (torch.abs(pred[:, 0] - gt) * mask).sum() / mask.sum()
-        return loss, dict(l1_loss=loss)
+        mask_sum = mask.sum()
+        if mask_sum.item() == 0:
+            return mask_sum, dict(l1_loss=mask_sum)
+        else:
+            loss = (torch.abs(pred[:, 0] - gt) * mask).sum() / mask_sum
+            return loss, dict(l1_loss=loss)
 
 
 class BalanceL1Loss(nn.Module):
