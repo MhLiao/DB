23391f33cef057aebf312e842cf48e203849b7ee
PKQ <zhutaonan@i-i.ai>
2019-12-05 16:57:22 +0800

    update

--- tmp/test_1.py
+++ tmp/test_1.py
@@ -2,7 +2,7 @@
 # @author :adolf
 import yaml
 
-with open('../experiments/seg_detector/totaltext_resnet50_deform_thre.yaml') as loadfile:
+with open('./experiments/seg_detector/totaltext_resnet50_deform_thre.yaml') as loadfile:
     p_yaml = yaml.load(loadfile)
 
 print(p_yaml)
