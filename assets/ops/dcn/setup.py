0e9f7856e3bed69dcadd0a4bad1823b8ad9d3991
phucpd53 <phucpd53@gmail.com>
2024-03-01 15:14:13 +0900

    check cuda available

--- assets/ops/dcn/setup.py
+++ assets/ops/dcn/setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
+import torch
 
-setup(
-    name='deform_conv',
-    ext_modules=[
+ext_modules = [
+    CppExtension('deform_conv_cpu', [
+        'src/deform_conv_cuda.cpp',
+        'src/deform_conv_cpu_kernel.cpp',
+    ]),
+    CppExtension('deform_pool_cpu', [
+        'src/deform_pool_cuda.cpp', 'src/deform_pool_cpu_kernel.cpp'
+    ]),
+]
+if torch.cuda.is_available():
+    ext_modules += [
         CUDAExtension('deform_conv_cuda', [
             'src/deform_conv_cuda.cpp',
             'src/deform_conv_cuda_kernel.cu',
@@ -11,12 +20,9 @@ setup(
         CUDAExtension('deform_pool_cuda', [
             'src/deform_pool_cuda.cpp', 'src/deform_pool_cuda_kernel.cu'
         ]),
-        CppExtension('deform_conv_cpu', [
-            'src/deform_conv_cuda.cpp',
-            'src/deform_conv_cpu_kernel.cpp',
-        ]),
-        CppExtension('deform_pool_cpu', [
-            'src/deform_pool_cuda.cpp', 'src/deform_pool_cpu_kernel.cpp'
-        ]),
-    ],
+        
+    ]
+setup(
+    name='deform_conv',
+    ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExtension})
