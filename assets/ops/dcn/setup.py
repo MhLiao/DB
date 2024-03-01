3df956d7809724e79daf27abee46163a4f03de38
phucpd53 <phucpd53@gmail.com>
2024-03-01 10:07:23 +0900

    build cpu conv

--- assets/ops/dcn/setup.py
+++ assets/ops/dcn/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 setup(
     name='deform_conv',
@@ -10,6 +10,13 @@ setup(
         ]),
         CUDAExtension('deform_pool_cuda', [
             'src/deform_pool_cuda.cpp', 'src/deform_pool_cuda_kernel.cu'
+        ]),
+        CppExtension('deform_conv_cpu', [
+            'src/deform_conv_cuda.cpp',
+            'src/deform_conv_cpu_kernel.cpp',
+        ]),
+        CppExtension('deform_pool_cpu', [
+            'src/deform_pool_cuda.cpp', 'src/deform_pool_cpu_kernel.cpp'
         ]),
     ],
     cmdclass={'build_ext': BuildExtension})
