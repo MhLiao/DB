803ba89fcd1a5dde20acabf69fa29f7e08570e96
phucpd53 <phucpd53@gmail.com>
2024-03-01 17:11:21 +0900

    refactoring

--- db_for_multiline.py
+++ db_for_multiline.py
@@ -75,14 +75,14 @@ def main():
             main_color = hist_sort_by_frequency[1][1]
             # second most pixel color as pen color
             pen_color = hist_sort_by_frequency[0][1]
-            if main_color > pen_color:
-                img = cv2.copyMakeBorder(img, args['pad'], args['pad'], args['pad'], args['pad'], cv2.BORDER_CONSTANT, None, (255, 255, 255))
-            else:
-                img = cv2.copyMakeBorder(img, args['pad'], args['pad'], args['pad'], args['pad'], cv2.BORDER_CONSTANT, None, (0, 0, 0))
+            # if main_color > pen_color:
+            #     img = cv2.copyMakeBorder(img, args['pad'], args['pad'], args['pad'], args['pad'], cv2.BORDER_CONSTANT, None, (255, 255, 255))
+            # else:
+            img = cv2.copyMakeBorder(img, args['pad'], args['pad'], args['pad'], args['pad'], cv2.BORDER_CONSTANT, None, (0, 0, 0))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = remove_background_imageset_by_opencv(gray)
         heat = CNN_forward(model, img.astype('float32'), args['image_short_side'])
-        all_boxes, all_rects, end_line_list = split_line_with_contour(gray, heat, args['box_thresh'])
+        all_boxes, all_rects, end_line_list = split_line_with_contour(heat, args['box_thresh'])
         for idx, (box, (x, y, w, h), is_endline) in enumerate(zip(all_boxes, all_rects, end_line_list)):
             cv2.polylines(img, [box], True, (0, 255, 0), 1)
             min_x = np.argmin(box[:, 0])
@@ -174,7 +174,7 @@ def check_min_rect_quality(min_rect, contour, overlap_ratio=0.8):
     return False
 
 
-def split_line_with_contour(img, heat, thresh):
+def split_line_with_contour(heat, thresh):
     if np.max(heat) > thresh:
         heat = cv2.equalizeHist((heat * 255).astype(np.uint8)).astype(np.float32) / 255
     binary = heat > thresh
@@ -186,71 +186,25 @@ def split_line_with_contour(img, heat, thresh):
         # return boxes as whole image
         return [np.array([
             [0, 0],
-            [img.shape[1], 0],
-            [img.shape[1], img.shape[0]],
-            [0, img.shape[0]]
-        ])], [(0, 0, img.shape[1], img.shape[0])], [False]
-    # max_height = max([cv2.boundingRect(contour)[3] for contour in contours])
-    # contours_map = [cv2.drawContours(np.zeros(img.shape, np.uint8), [contour], 0, 255, cv2.FILLED) for contour in contours]
-
-    # # determine how should we expand the contour to make them intersect
-    # kernel = np.array([
-    #     [1, 1, 1],
-    #     [0, 1, 0],
-    #     [1, 1, 1]
-    # ], dtype=np.uint8) # dilate on vertical only
-    # iterations_list = [9999] * len(contours)
-    # print(len(contours))
-    # #TODO: can optimize the speed here
-    # for idx1, idx2 in itertools.combinations(range(len(contours)), 2):
-    #     map1 = contours_map[idx1]
-    #     map2 = contours_map[idx2]
-
-    #     # check if 2 contours can overlap in x direction
-    #     x1, y1, w1, h1 = cv2.boundingRect(contours[idx1])
-    #     x2, y2, w2, h2 = cv2.boundingRect(contours[idx2])
-    #     if min(x1 + w1, x2 + w2) - max(x1, x2) < -3:
-    #         continue
-    #     if min(y1 + h1, y2 + h2) - max(y1, y2) > -3 or min(y1 + h1, y2 + h2) - max(y1, y2) < - min(h1, h2):
-    #         continue
-
-    #     map12 = cv2.bitwise_or(map1, map2)
-    #     for iteration in range(1, min(h1, h2, w1, w2)):
-    #         map12 = cv2.dilate(map12, kernel, 1)
-    #         contours_tmp, _ = cv2.findContours(map12, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #         if len(contours_tmp) < 2:
-    #             iterations_list[idx1] = min(iterations_list[idx1], iteration - 1)
-    #             iterations_list[idx2] = min(iterations_list[idx2], iteration - 1)
-    #             break
-    # # expand the contour right just before they intersect to each other
-    # adjusted_contours = []
-    # for contour, contour_map, iterations in zip(contours, contours_map, iterations_list):
-    #     dilated = cv2.dilate(contour_map, np.ones((3,3)), iterations=iterations)
-    #     contours_tmp, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #     if len(contours_tmp) < 1:
-    #         assert "number of contour should be more than 1"
-    #     adjusted_contours.append(contours_tmp[0])
-    # contours = adjusted_contours
-    # # hoge = np.dstack([img.copy()] * 3)
-    # # cv2.drawContours(hoge, contours_tmp, -1, (0, 255, 0), 1)
-    # # cv2.imwrite("hoge.png", hoge)
-    # # exit()
+            [heat.shape[1], 0],
+            [heat.shape[1], heat.shape[0]],
+            [0, heat.shape[0]]
+        ])], [(0, 0, heat.shape[1], heat.shape[0])], [False]
+    
     original_boxes = []
     all_boxes = []
     all_rects = []
     for contour in contours:
         # straight rectangle
         x, y, w, h = cv2.boundingRect(contour)
-        # piece = heat[y:y+h, x:x+w]
         if min(w, h) < 5 or w * h < 50:
             continue
         
-        contour = adjust_corner(contour, img_shape=img.shape, kernel_size=h)
+        contour = adjust_corner(contour, img_shape=heat.shape, kernel_size=h)
 
         # straight rectangle
         x, y, w, h = cv2.boundingRect(contour)
-        # piece = heat[y:y+h, x:x+w]
-
+        
         # skew rectangle
         bounding_box = cv2.minAreaRect(contour)
         # poly
